50f352b0ccc23608cf18fdafd993b69da4a3c9be
Pranav Pawar <pranavpawar775@gmail.com>
2024-07-17 13:09:08 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/section/Hero";
+import Pricing from "./components/section/Pricing";
+import Circle from "./components/elements/Circle";
+import Features from "./components/section/Features";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative px-32 bg-white font-['neue']">
+      <Navbar />
+      {/* <Circle/> */}
+      <Hero/>
+      <Features/>
+      <Pricing/>
     </div>
   );
-}
+};
 
 export default App;
